64ad4306add8d605daf491ebfbe7cba453ebfa62
Jacob Liou <jacob.c.liou@gmail.com>
2016-09-15 10:00:51 -0500

    Update Homework 1_Problem2.cpp

--- Homework 1_Problem2.cpp
+++ Homework 1_Problem2.cpp
@@ -4,9 +4,42 @@
 using namespace std;
 
 void bubbleSort(int [], int);
-void selectionSort(int[], int);
-void resetA(int [], int);
+void createRandomArray(int[], int);
 int main()
 {
-
+  int size;
+  std::cout<<"Please enter the size of the array";
+  std::cin >> size;
+  int rand[size];
+  createRandomArray(rand, size);
+  
+}
+void bubbleSort(int array[], int size)
+{
+    bool swap;
+    int temp;
+    do
+    {
+        swap = false;
+        for(int count = 0; count < (size-1); count++)
+        {
+            if (array[count] > array[count + 1])
+            {
+            temp = array[count];
+            array[count] = array[count + 1];
+            array[count + 1] = temp;
+            swap = true;
+            }
+            for(int i=0; i<size; i++)
+            {
+                std::cout<<array[i]<<" ";
+            }
+            std::cout<<"\n";
+        }
+    } while (swap);
+    std::cout<<"Sorted!"<<"\n";
+}
+void createRandomArray(int array[], int size);
+{
+  
 }
