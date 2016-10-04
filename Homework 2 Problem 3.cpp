1848c4c187d61fc1adb08e332fdcad78d86ed278
Jacob Liou <jacob.c.liou@gmail.com>
2016-10-04 10:43:15 -0500

    Update Homework 2 Problem 3.cpp

--- Homework 2 Problem 3.cpp
+++ Homework 2 Problem 3.cpp
@@ -17,7 +17,7 @@ int main() {
 	string input;
 	int counter = 1;
 	int looper = 0;
-	Candidate c[100];
+	Candidate c[20];
 	char temp[100];
 	file.open("candidatedata.txt");
         while(!file.eof())
@@ -40,9 +40,19 @@ int main() {
             looper++;
         }
         file.close();
-        for(int i=0; i<100; i++)
+        /*for(int i=0; i<20; i++)
         {
             std::cout<<"\n"<<c[i].name<<" "<<c[i].gender<<" "<<c[i].height<<" "<<c[i].weight<<"\n";
+        }*/
+        int females =0;
+        float fper;
+        for(int i=0; i<20; i++)
+        {
+            if(c[i].gender=='F')
+                females++;
         }
+        fper = (float)females/20;
+        std::cout<<"Number of female and male candidates respectively: "<<females<<" "<<20-females;
+        std::cout<<"\nPercentage of female and male candidates respectively: "<<fper<<" "<<(1-fper);
 
 }
