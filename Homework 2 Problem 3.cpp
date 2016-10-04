42cdf2196b4484ba73963de28128fd520ade1a80
Jacob Liou <jacob.c.liou@gmail.com>
2016-10-04 10:29:15 -0500

    Update Homework 2 Problem 3.cpp

--- Homework 2 Problem 3.cpp
+++ Homework 2 Problem 3.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 using namespace std;
 struct Candidate{
     string name;
-    string gender;
+    char gender;
     double height;
     double weight;
 };
@@ -15,34 +17,32 @@ int main() {
 	string input;
 	int counter = 1;
 	int looper = 0;
-	Candidate c[40];
+	Candidate c[100];
+	char temp[100];
 	file.open("candidatedata.txt");
-    getline(file,input);
-        while(file)
+        while(!file.eof())
         {
             getline(file,input,',');
-            std::cout<<input<<std::endl;
+            c[looper].name=input;
 
-            switch(counter)
-            {
-                case 1:
-                    counter++;
-                    //c[looper].name = input;
-                    break;
-                case 2:
-                    counter++;
-                    //c[looper].gender= input;
-                    break;
-                case 3:
-                    counter++;
-                    //c[looper].height = atof(input);
-                    break;
-                case 4:
-                    counter = 1;
-                    //c[looper].weight = atof(input);
-                    break;
-            }
+            getline(file,input,',');
+            strcpy(temp,input.c_str());
+            c[looper].gender=temp[0];
+
+            getline(file,input,',');
+            strcpy(temp,input.c_str());
+            c[looper].height=atof(temp);
+
+            getline(file,input);
+            strcpy(temp,input.c_str());
+            c[looper].weight=atof(temp);
+
             looper++;
         }
         file.close();
+        for(int i=0; i<100; i++)
+        {
+            std::cout<<"\n"<<c[i].name<<" "<<c[i].gender<<" "<<c[i].height<<" "<<c[i].weight<<"\n";
+        }
+
 }
