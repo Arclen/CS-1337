a5b9e3e0928ea2bec71231bb5463cd05c012a073
Jacob Liou <jacob.c.liou@gmail.com>
2016-10-25 12:01:40 -0500

    Update Homework 3 Problem 1.cpp

--- Homework 3 Problem 1.cpp
+++ Homework 3 Problem 1.cpp
@@ -2,11 +2,12 @@ using namespace std;
 #include <iostream>
 #include "Student.h"
 #include <string>
+#include <stdlib.h>
 
 
 int main()
 {
     Student stu;
-    stu.setIQ(3.3);
+   // stu.setIQ(3);
     cout<<stu.getIQ();
 }
