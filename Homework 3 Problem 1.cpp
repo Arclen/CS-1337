cb796fac6a5627d9a72852dc0e99b3f8a5c59a6b
Jacob Liou <jacob.c.liou@gmail.com>
2016-10-26 18:45:58 -0500

    Update Homework 3 Problem 1.cpp

--- Homework 3 Problem 1.cpp
+++ Homework 3 Problem 1.cpp
@@ -1,12 +1,15 @@
 using namespace std;
 #include <iostream>
 #include "Student.h"
+#include "Campus.h"
 #include <string>
 #include <stdlib.h>
 
 
 int main()
 {
+    Campus camp(25, 15);
+    cout<<camp;
     Student students[10];
     for(int i=0; i<10; i++)
         cout<<students[i]<<"\n";
