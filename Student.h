733efe19c46792edc152c9e59e96c60fbc891226
Jacob Liou <jacob.c.liou@gmail.com>
2016-10-31 12:51:32 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -45,7 +45,27 @@ class Student{
 
         void studentMove()
         {
+            //ask Prof Feng if in future, students can be in trash space
             int i = rand() % 4 + 1;
+            switch(i)
+            {
+            case 1:
+                if(xpos>0)
+                    xpos--;
+                break;
+            case 2:
+                if(ypos>0)
+                    ypos--;
+                break;
+            case 3:
+                if(xpos<25)
+                    xpos++;
+                break;
+            case 4:
+                if(ypos<25)
+                    ypos++;
+                break;
+            }
         }
 
         friend ostream& operator<<(ostream& os, const Student &stu)
